c0fcbda3b1303f4cd9dde7716e09184f385d8019
7BF794B0 <eremchenko.au@gmail.com>
2020-03-10 15:38:03 +0300

    Add EndPointImplementation

---
+++ JsonDownloader/Implementations/EndPointImplementation.cs
@@ -0,0 +1,22 @@
+﻿using JsonDownloader.Interfaces;
+
+namespace JsonDownloader.Implementations
+{
+    class EndPointImplementation : IEndPoint
+    {
+        public IUrl GetUrl()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDownloadStrategy GetDownloadStrategy()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IStatusCode Download(IDownloadStrategy strategy, IUrl toDownload)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
