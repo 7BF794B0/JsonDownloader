804264ac61b30a52f8abad4301242219696d0dad
7BF794B0 <eremchenko.au@gmail.com>
2020-03-10 14:52:02 +0300

    Add Url class

---
+++ JsonDownloader/Entities/Url.cs
@@ -0,0 +1,17 @@
+﻿namespace JsonDownloader.Entities
+{
+    class Url
+    {
+        private string url;
+
+        public string GetUrl()
+        {
+            return url;
+        }
+
+        private void SetUrl(string value)
+        {
+            url = value;
+        }
+    }
+}
