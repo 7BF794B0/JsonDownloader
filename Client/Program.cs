9df1a1f32594783a33364fa5522eda7a99258c87
7BF794B0 <eremchenko.au@gmail.com>
2020-04-01 13:51:16 +0300

    Update Client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JsonDownloader;
 
 namespace Client
 {
@@ -10,7 +6,7 @@ namespace Client
     {
         static void Main(string[] args)
         {
-
+            Manager mng = new Manager();
         }
     }
 }
