85a10460e9dacb644cf51ae1f85bc5dffbaffb4a
7BF794B0 <eremchenko.au@gmail.com>
2020-04-01 14:50:38 +0300

    Add StatusCodeImplementation

---
+++ JsonDownloader/Entities/StatusCodeImplementation.cs
@@ -0,0 +1,19 @@
+﻿using JsonDownloader.Interfaces;
+
+namespace JsonDownloader.Entities
+{
+    class StatusCodeImplementation : IStatusCode
+    {
+        public int Code { get; private set; }
+
+        public StatusCodeImplementation(int code)
+        {
+            Code = code;
+        }
+
+        public int GetStatusCode()
+        {
+            return Code;
+        }
+    }
+}
