a5fdbbc4dc92529c81b79d16915f8c694b62c4d2
7BF794B0 <eremchenko.au@gmail.com>
2020-04-01 13:35:14 +0300

    Update Manager constructor

--- JsonDownloader/Manager.cs
+++ JsonDownloader/Manager.cs
@@ -1,10 +1,29 @@
-﻿using JsonDownloader.Factories;
+﻿using JsonDownloader.Exceptions;
+using JsonDownloader.Factories;
+using JsonDownloader.Interfaces;
 
 namespace JsonDownloader
 {
     public class Manager
     {
-        EndPointFactory factory = EndPointFactory.GetInstance();
+        private static Manager instance;
 
+        public static void instantiateManagerAndRun()
+        {
+            instance = new Manager();
+        }
+
+        public Manager()
+        {
+            EndPointFactory factory = EndPointFactory.GetInstance();
+            IEndPoint endPoint = factory.CreateEndPoint();
+            IUrl url = endPoint.GetUrl();
+            IDownloadStrategy downloadStrategy = endPoint.GetDownloadStrategy();
+            IStatusCode code = endPoint.Download(downloadStrategy, url);
+            if (code.GetStatusCode() != 0)
+            {
+                throw new RuntimeException("Failed to download: " + code.GetStatusCode());
+            }
+        }
     }
 }
